133044a5d1b3b19f7684cceca6d4293b0e705c6f
lily <lucky.lapras12@gmail.com>
2022-07-02 16:07:11 +0100

    fix lock script to not lock when charging

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -10,24 +10,26 @@ export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
 # Run xidlehook only when discharging
 BATS=/sys/class/power_supply/BAT0/status
+status=$(cat $BATS)
 
-while [ "$(cat $BATS)" == "Discharging" ]; do
-    xidlehook \
-      `# Don't lock when there's a fullscreen application` \
-      --not-when-fullscreen \
-      `# Don't lock when there's audio playing` \
-      --not-when-audio \
-      `# Dim the screen after 60 seconds, undim if user becomes active` \
-      --timer 60 \
-        'xbacklight -50' \
-        'xbacklight +50' \
-      `# Undim & lock after 10 more seconds` \
-      --timer 10 \
-        'i3lock -i /home/lily/Pictures/Wallpapers/yliaed.png; sleep 1; xbacklight +50' \
-        '' \
-      `# Finally, suspend an hour after it locks` \
-      --timer 300 \
-        'systemctl suspend' \
-        ''
+while [ "$status" == "Discharging" ]; do
+    status=$(cat $BATS)
+#    xidlehook \
+#      `# Don't lock when there's a fullscreen application` \
+#      --not-when-fullscreen \
+#      `# Don't lock when there's audio playing` \
+#      --not-when-audio \
+#      `# Dim the screen after 60 seconds, undim if user becomes active` \
+#      --timer 60 \
+#        'xbacklight -50' \
+#        'xbacklight +50' \
+#      `# Undim & lock after 10 more seconds` \
+#      --timer 10 \
+#        'i3lock -i /home/lily/Pictures/Wallpapers/yliaed.png; sleep 1; xbacklight +50' \
+#        '' \
+#      `# Finally, suspend an hour after it locks` \
+#      --timer 300 \
+#        'systemctl suspend' \
+#        ''
 done
 #e
