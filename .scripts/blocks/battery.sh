7f1263c24558a36836ac2e197f10eb14dd73cd2d
lily <lucky.lapras12@gmail.com>
2022-07-05 00:17:43 +0100

    minor edit to picom, rewrote battery using case instead of if, some edit to wifi that idr

--- .scripts/blocks/battery.sh
+++ .scripts/blocks/battery.sh
@@ -1,76 +1,63 @@
 #!/bin/dash
 
+label() {
+    label="%%{F#ec3257}"$1"%%{F-}"
+}
 
 battery() {
     BATC=/sys/class/power_supply/BAT0/capacity
     BATS=/sys/class/power_supply/BAT0/status
 
-#test "`cat $BATS`" = "Charging" && echo -n '+' || echo -n '-'
+    #need to set an arbitrarily high NID for dunst
+    #maybe it doesn't need to be arbitrarily high i just thought it'd be safer
+    NID=420
+    #(i choose the funny weed number :3)
 
-    number=$(cat $BATC)
-
-#echo $number
+    #define some colours
+    norm="#ec3257"
+    crit="#cd1338"
+    char="#86dcfd"
 
     uoline=u
 
     string=""
     string="$string%%{+$uoline}"
 
-    underline=""
+    underline=$norm
 
-    if [ $(cat $BATC) -le 10 ];
-    then
-        underline="#cd1338"
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 20 ];
-    then
-        underline="#cd1338"
-        label="%%{F#ec3257} %%{F-}"
-        if [ "$(cat $BATS)" == "Discharging" ];
-        then
-            NID=$(dunstify -u critical -p 'i dont care if u transgender' 'charge yo damn laptop')
-        fi
-    elif [ $(cat $BATC) -le 30 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 40 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 50 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 60 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 70 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 80 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    elif [ $(cat $BATC) -le 98 ];
-    then
-        label="%%{F#ec3257} %%{F-}"
-    else
-        label="%%{F#ec3257} %%{F-}"
-    fi
+    case $(cat $BATC) in
+        [0-9]|1[0-9])  underline=$crit
+            label=$(label ' ')
+            case $(cat $BATS) in
+                Discharging) dunstify -u critical -r $NID 'i dont care if u transgender' 'charge yo damn laptop'
+            esac
+            ;;
+        2[0-9]|3[0-9]) label=$(label ' ')
+            ;;
+        4[0-9]|5[0-9]) label=$(label ' ')
+            ;;
+        6[0-9]|7[0-9]) label=$(label ' ')
+            ;;
+        8[0-9]|9[0-9]) label=$(label ' ')
+            ;;
+        *)             label=$(label ' ')
+            ;;
+    esac
 
-    if [ "$(cat $BATS)" = "Charging" ] || [ "$(cat $BATS)" = "Unknown" ];
-    then
-        dunstify -C $NID
-        underline="#86dcfd"
-    elif [ "$(cat $BATS)" = "Full" ];
-    then
-        dunstify -r $NID 'i care that u transgender' 'u can stop charging yo damn laptop'
-        underline="#86dcfd"
-    else
-        dunstify -C $NID
-    fi
-
+    case $(cat $BATS) in
+        Charging|Unknown) dunstify -C $NID
+            underline=$char
+            ;;
+        Full)             dunstify -r $NID 'i care that u transgender' 'u can stop charging yo damn laptop'
+            underline=$char
+            ;;
+        *)                dunstify -C $NID
+            ;;
+    esac
 
     string="$string%%{U$underline}$label"
 
-    string="$string$number"
+    string="$string$(cat $BATC)"
 
     string="$string%%%%"
 
--- .scripts/blocks/wifi.sh
+++ .scripts/blocks/wifi.sh
@@ -1,18 +1,17 @@
 #!/bin/dash
 
 wifi() {
-	int="wlp2s0"
-	SSID=$(iw "$int" info | awk '/ssid/ {$1=""; print $0}' | sed 's/^.//')
+    int="wlp2s0"
+    SSID=$(iw "$int" info | awk '/ssid/ {$1=""; print $0}' | sed 's/^.//')
 
-	ping -c 1 8.8.8.8 >/dev/null 2>&1 && printf '%%{+u}%%{U#f7a8b8}%%{F#ec3257}直 %%{F-}'$SSID'%%{-u}'\\n || printf '%%{+u}%%{U#cccccc}%%{F#ec3257}睊 Disconnected%%{U-}%%{-u}'\\n
+    ping -c 1 8.8.8.8 >/dev/null 2>&1 && printf '%%{+u}%%{U#f7a8b8}%%{F#ec3257}直 %%{F-}'$SSID'%%{-u}'\\n || printf '%%{+u}%%{U#cccccc}%%{F#ec3257}睊 Disconnected%%{U-}%%{-u}'\\n
 }
 
 trap 'wifi' 42
 
-wifi
-
 while :;
 do
-	sleep infinity &
-	wait
+    wifi
+    sleep 60 &
+    wait
 done
