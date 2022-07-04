14c6251511b3413e2aa32a0951c6b7a3a0fadadf
lily <lucky.lapras12@gmail.com>
2022-07-04 20:11:57 +0100

    update lock script again (hopefully for the last time)

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -7,11 +7,11 @@ xidlehook \
   --not-when-audio \
   `# Dim the screen after 60 seconds, undim if user becomes active` \
   --timer 60 \
-    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && xbacklight -35' \
-    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && xbacklight +35' \
-    `# Undim & lock after 10 more seconds (i would like to be able to have a brightness variable here and return the screen to the brightness it was at before instead of forcing a 35% change (i doubt i'll have my screen any lower than that) but setting a variable inside the time doesn't seem to work. i could set it outside the xidlehook command but then it would be set at the start of the session` \
-  --timer 10 \
-    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && i3lock -i /home/lily/Pictures/Wallpapers/yliaed.png && sleep 1&& xbacklight +35' \
+    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && xbacklight -get > /tmp/bness.txt &&  xbacklight =1' \
+    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && xbacklight =$(cat /tmp/bness.txt)' \
+    `# Undim & lock after 30 more seconds` \
+  --timer 30 \
+    'grep 0 /sys/class/power_supply/ADP0/online > /dev/null && i3lock -i /home/lily/Pictures/Wallpapers/yliaed.png && sleep 1&& xbacklight =$(cat /tmp/bness.txt)' \
     '' \
   `# Finally, suspend an hour after it locks` \
   --timer 300 \
