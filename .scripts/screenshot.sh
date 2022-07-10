a224e3ce889eef02a75ce5e3b552580ee1317352
lily <lucky.lapras12@gmail.com>
2022-07-10 15:58:09 +0100

    update screenshot script

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -6,31 +6,31 @@ mkdir -p $DIR
 export MESSAGE="File saved to $DIR/$FILENAME"
 
 notify() {
-	notify-send -i $DIR/$FILENAME "Screenshot taken!" "$MESSAGE"
+`notify-send -i $DIR/$FILENAME "Screenshot taken!" "$MESSAGE"
 }
 
 selection() (
-	# take screenshot without cursor and pipe to feh, get its pid
-	maim -u | feh - &
-	feh_pid=$!
+    # take screenshot without cursor and pipe to feh, get its pid
+    maim -u | feh - &
+    feh_pid=$!
 
-	# wait for feh to start
-	while [ -z "$(xdotool search --pid "$feh_pid")" ]; do
-		sleep 0.1
-	done
+    # wait for feh to start
+    while [ -z "$(xdotool search --pid "$feh_pid")" ]; do
+        sleep 0.1
+    done
 
-	# get window ID of feh
-	wid="$(xdotool search --pid "$feh_pid")"
+    # get window ID of feh
+    wid="$(xdotool search --pid "$feh_pid")"
 
-	# fullscreen feh and move top-left (works with multi-monitor)
-	xdotool windowsize "$wid" 100% 100%
-	xdotool windowmove "$wid" 0 0
+    # fullscreen feh and move top-left (works with multi-monitor)
+    xdotool windowsize "$wid" 100% 100%
+    xdotool windowmove "$wid" 0 0
 
-	# take the new screenshot by selection, pipe to clipboard
-	maim -s -u "$DIR/$FILENAME"
+    # take the new screenshot by selection, pipe to clipboard
+    maim -s -u "$DIR/$FILENAME"
 
-	# kill feh
-	kill "$feh_pid"
+    # kill feh
+    kill "$feh_pid"
 )
 
 while getopts ":qaw" option; do
@@ -42,7 +42,7 @@ while getopts ":qaw" option; do
            exit;;
         a) #maim -s -u $DIR/$FILENAME
            selection
-           if [ $? -eq 0 ]; then
+           if [ -f $DIR/$FILENAME ]; then
                xclip -sel clip < /dev/null
                xclip -selection clip -t image/png $DIR/$FILENAME
                notify
