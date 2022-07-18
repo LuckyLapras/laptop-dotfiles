a9f6d3ae1a5a540c81d0cf3573597075d54a1b8c
lily <lucky.lapras12@gmail.com>
2022-07-18 20:27:29 +0100

    edit readme

--- .scripts/blocks/refresh_battery.sh
+++ .scripts/blocks/refresh_battery.sh
@@ -7,7 +7,7 @@ pkill -RTMIN+10 battery.sh
 BATS=/sys/class/power_supply/BAT0/status
 
 case $(cat $BATS) in
-    Charging|Unknown|Full) xbacklight -get > /tmp/bness.txt && xbacklight =1
+    Charging|Unknown|Full) xbacklight -get > /tmp/bness.txt && xbacklight =100
         ;;
     *) xbacklight =$(cat /tmp/bness.txt)
 esac
