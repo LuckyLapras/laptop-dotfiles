4205228d78b24d72a8d1fbfeac530d043dae0ef9
lily <lucky.lapras12@gmail.com>
2022-07-18 10:36:58 +0100

    add battery refresh script

--- .scripts/blocks/refresh_battery.sh
+++ .scripts/blocks/refresh_battery.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#call this scripts in a udev rule
+
 pkill -RTMIN+10 battery.sh
+
+BATS=/sys/class/power_supply/BAT0/status
+
+case $(cat $BATS) in
+    Charging|Unknown|Full) xbacklight -get > /tmp/bness.txt && xbacklight =1
+        ;;
+    *) xbacklight =$(cat /tmp/bness.txt)
+esac
+
