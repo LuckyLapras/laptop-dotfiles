dfd6539a2258bf72e4906fd7afe3086c96321842
lily <lucky.lapras12@gmail.com>
2023-05-10 23:06:26 +0100

    yup

--- .scripts/blocks/refresh_battery.sh
+++ .scripts/blocks/refresh_battery.sh
@@ -6,8 +6,11 @@ pkill -RTMIN+10 battery.sh
 
 BATS=/sys/class/power_supply/BAT0/status
 
+# we sleep here bc my charger sucks and gets knocked out very easily which fucks up the saving process
+# (i'm sick of my brightness getting set to 100 randomly without me realising)
+
 case $(cat $BATS) in
-    Charging|Unknown|Full|"Not charging") brightnessctl -s && brightnessctl s $(brightnessctl max)
+    Charging|Unknown|Full|"Not charging") sleep 5 && $HOME/.local/bin/save_backlight.sh save && $HOME/.local/bin/save_backlight.sh load
         ;;
-    *) brightnessctl -r
+    *) sleep 5 && $HOME/.local/bin/save_backlight.sh load && $HOME/.local/bin/save_backlight.sh save
 esac
